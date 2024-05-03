2a90934d1bc819abc447e54a0ff4e111c0d8167a
KEZINHADOGRAU <106972304+KEZINHADOGRAU@users.noreply.github.com>
2024-05-03 07:25:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,12 +8,10 @@ for (let i = 0; i < botoes.length; i++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
+    botoes[i].classList.add("ativo");
+   textos[i].classList.add("ativo");
     }
 }
-
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-06T00:00:00");
 const tempoObjetivo2 = new Date("2024-11-03T00:00:00");
@@ -21,7 +19,6 @@ const tempoObjetivo3 = new Date("2024-11-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-12-20T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
